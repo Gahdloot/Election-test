842eb92b01e688ca272ee0d3ba7a27fe879fa0d3
Michael David <davidlearn27@gmail.com>
2023-03-31 13:54:21 +0100

    Change directory for deployment

--- election/core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import polling_unit, total_res_lga
-
-urlpatterns = [
-    path('unit/<int:id>', polling_unit),
-    path('total_res_lga/', total_res_lga)
-]
